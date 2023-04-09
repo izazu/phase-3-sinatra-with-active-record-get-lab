dd946f919b2b1e5587ff940590e6a5497d5001bd
isaiahsikobe <isaiah.sikobe@student.moringaschool.com>
2023-04-09 19:04:03 +0200

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  get '/bakeries' do
+    content_type :json
+    bakeries = Bakery.all
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/bakeries/:id' do
+    content_type :json
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    content_type :json
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    content_type :json
+    baked_good = BakedGood.order(price: :desc).first
+    baked_good.to_json
+  end
 
 end
+
